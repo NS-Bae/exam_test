7c899af3e6ad9d57a6b6a30e97aa2dc7c14043e6
NS-Bae <qw9711@gmail.com>
2023-05-23 14:48:28 +0900

    학습2

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'polls.apps.PollsConfig', 
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'web',
+        'USER': 'postgres',
+        'PASSWORD': 'admin', 
+        'HOST': 'localhost', 
+        'PORT': '5432',
     }
 }
 
--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,33 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.http import HttpResponse, Http404
+from .models import Question
+from django.template import loader
 
 def index(request):
-    return HttpResponse("Hello, World. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {'latest_question_list' : latest_question_list, }
+    #output = ', '.join([q.question_text for q in latest_question_list])
+    #httpresponse활용하여 출력하기
+    #return HttpResponse(template.render(context, request))
+    #render함수 활용하여 출력하기.
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    #404 error 처리
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question' : question})
+    #return HttpResponse("당신은 지금 질문을 보고 있습니다. %s ." %question_id)
+
+def results(request, question_id):
+    response = "당신은 지금 질문의 결과를 보고 있습니다. %s ."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("당신은 지금 질문에 투표중입니다. %s" % question_id)
+
+
 # Create your views here.
